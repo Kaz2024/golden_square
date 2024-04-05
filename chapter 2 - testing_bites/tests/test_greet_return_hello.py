09dec96279af1725f21cf042a7cfbaf37b265d9c
Kathey Zhang <kathey.qianlin.zhang@gmail.com>
2024-04-05 12:47:22 +0100

    Remove deleted files from from the project

--- chapter 2 - testing_bites/tests/test_greet_return_hello.py
+++
@@ -1,5 +0,0 @@
-from lib.greet import *
-
-def test_greet_return_hello():
-    result = greet("Kathey")
-    assert result ==  "Hello, Kathey!"
--- chapter 2 - testing_bites/tests/test_reminder.py
+++
@@ -1,7 +0,0 @@
-from lib.reminder import *
-
-def test_reminds_the_user_to_do_a_task():
-    reminder = Reminder("Zoe")
-    reminder.remind_me_to("Walk the cat")
-    result = reminder.remind()
-    assert result == "Walk the cat, Zoe"
