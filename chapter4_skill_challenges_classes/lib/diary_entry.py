1031863ea8f10340f084d55486c5e59829016c0c
Kathey Zhang <kathey.qianlin.zhang@gmail.com>
2024-04-10 19:24:43 +0100

    final commit

---
+++ chapter4_skill_challenges_classes/lib/diary_entry.py
@@ -0,0 +1,39 @@
+class DiaryEntry:
+    def __init__(self, title, contents):
+        if title =="" and contents == "":
+            raise Exception("Diary entries must have a title or contents.")
+        self.title = title
+        self.contents = contents
+        
+    def format(self):
+        return f"{self.title}: {self.contents}"
+        pass
+
+    def count_words(self):
+        words = self.format().split()
+        return len(words)
+        pass
+
+    def reading_time(self, wpm):
+        # Parameters:
+        #   wpm: an integer representing the number of words the user can read 
+        #        per minute
+        # Returns:
+        #   int: an estimate of the reading time in minutes for the contents at
+        #        the given wpm.
+        pass
+
+    def reading_chunk(self, wpm, minutes):
+        # Parameters
+        #   wpm: an integer representing the number of words the user can read
+        #        per minute
+        #   minutes: an integer representing the number of minutes the user has
+        #            to read
+        # Returns:
+        #   string: a chunk of the contents that the user could read in the
+        #           given number of minutes
+        #
+        # If called again, `reading_chunk` should return the next chunk,
+        # skipping what has already been read, until the contents is fully read.
+        # The next call after that should restart from the beginning.
+        pass
---
+++ chapter4_skill_challenges_classes/tests/test_diary_entry.py
@@ -0,0 +1,35 @@
+from lib.diary_entry import *
+import pytest
+# Given an empty title and contents
+# Return an error 
+
+def test_empty_string_return_error():
+    with pytest.raises(Exception) as err:
+        DiaryEntry("", "")
+        assert str(err.value) == "Diary entries must have a title or contents."
+
+
+# Given a title and contents
+#format returns a formatted entry, like:
+# "My Title: These are the contents"
+
+def test_formats_with_title_and_contents():
+    diary_entry = DiaryEntry("My Title", "Some contents")
+    result = diary_entry.format()
+    assert result == "My Title: Some contents"
+
+# given a title and content
+# count words returns the number of words in the title and contents
+
+def test_gets_count_of_title_and_contents():
+    diary_entry = DiaryEntry("My Title", "Some contents")
+    result = diary_entry.count_words()
+    assert result == 4
+
+# Give the title and contents
+# return the reading per minute 
+
+def test_reading_estimate():
+    diary_entry = DiaryEntry("My Title", "Some contents")
+    result = diary_entry.reading_time(2)
+    assert result == 1
